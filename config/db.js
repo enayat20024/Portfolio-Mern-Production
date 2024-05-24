ee43f2c141fc0538a785848c3a7231e75a6cb243
Enayatullah <enayatkhalili182@gmail.com>
2024-05-24 23:37:08 +0500

    update configuration for db
    please do it

--- config/db.js
+++ config/db.js
@@ -3,11 +3,8 @@ const MONGO_URL = process.env.MONGO_URL;
 
 const connectDB = async () => {
   try {
-    const con = await mongoose.connect(MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB connected : ${con.connection.host}`);
+    const con = await mongoose.connect(MONGO_URL);
+    console.log(`MongoDB connected: ${con.connection.host}`);
   } catch (err) {
     console.log(err);
     process.exit(1);
