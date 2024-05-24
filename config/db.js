086d64423744b9c7fbc2b1182c5b75adf746dea0
Enayatullah <enayatkhalili182@gmail.com>
2024-05-24 22:58:59 +0500

    production app
    mern stack portfolio production app 2024

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const MONGO_URL = process.env.MONGO_URL;
+
+const connectDB = async () => {
+  try {
+    const con = await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected : ${con.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
